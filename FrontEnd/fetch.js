5cbe2d77a8b573a3692528ba3d7914f0a5f48c82
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-05-22 11:48:31 +0200

    Add filter buttons

--- FrontEnd/fetch.js
+++ FrontEnd/fetch.js
@@ -1,34 +1,82 @@
+// Les projets s’affichent dans la galerie en provenant du back-end et en récupérant : .imagesURL et .title
+
 /**
- * (fetch...) Initie une requête réseau avec Url de API work GET pour récupérer les données
- * (.then(res...)) Convertit les données dans url au format JSON
- * (.then(data...))
- * (Const gallery) Assigne à la classe .gallery la variable galleryElemen
- * (data.forEach) Itére sur chaque élément projet dans le tableau de données
- * (.catch(error => {) Gère les erreurs
+ * (fetch...) Lance une requête réseau vers Url de API work GET pour récupérer les données 
+ *            et renvoie une promesse 
+ * (.then(response...)) Convertit les données dans url au format JSON
+ * (.then(datafiltre...)) datafiltre représente les données JSON extraite
  */
 
-fetch('http://localhost:5678/api/works') 
-.then(response => response.json())
-.then(data => {
-  const galleryElement = document.querySelector('.gallery');
-  
-  data.forEach(project => {
-    const figureElement = document.createElement('figure');
 
-    const imgElement = document.createElement('img');
-    imgElement.src = project.imageUrl;
-    imgElement.alt = project.title;
+
+fetch('http://localhost:5678/api/works') 
+  .then(response => response.json())
+  .then(datafiltre => {
+    const galleryElement = document.querySelector('.gallery');
+    //Assigne à ma classe gallery une variable
     
-    const figcaptionElement = document.createElement('figcaption');
-    figcaptionElement.textContent = project.title;
-    
-    figureElement.appendChild(imgElement);
-    figureElement.appendChild(figcaptionElement);
-    
-    galleryElement.appendChild(figureElement);
-  });
-})
-.catch(error => {
-  console.error('An error occurred while retrieving the projects:', error);
+    function displayProjects(projects) {
+      galleryElement.innerHTML = ''; 
+      // Efface la galerie existante pour assurer que seule la catégorie séléctionné est afficher.
+  
+      projects.forEach(project => {
+        //Boucle itère sur chaque élément du projects
+        const figureElement = document.createElement('figure');
+  
+        const imgElement = document.createElement('img');
+        imgElement.src = project.imageUrl;
+        imgElement.alt = project.title;
+        
+        const figcaptionElement = document.createElement('figcaption');
+        figcaptionElement.textContent = project.title;
+        
+        figureElement.appendChild(imgElement);
+        figureElement.appendChild(figcaptionElement);
+        
+        galleryElement.appendChild(figureElement);
+      });
+    }
+  
+    // Fonction pour filtrer les projets par catégorie
+    function filterName(categoryId) {
+      if (categoryId === null) {
+        displayProjects(datafiltre); // Afficher tous les projets
+      } else {
+        const filteredProjects = datafiltre.filter(project => project.categoryId === categoryId);
+        displayProjects(filteredProjects); // Afficher les projets filtrés
+      }
+    }
+  
+    // Créer les boutons de filtre
+    const filtre = document.querySelector(".filtre");
+    const uniqueFiltre = new Set();
+  
+    datafiltre.forEach(project => {
+      if (!uniqueFiltre.has(project.category.name)){
+        uniqueFiltre.add(project.category.name);
+        // ! = opérateur négation qui inverse la valeur true or false
+        //permet de vérifier que le nom n'est pas déjà afficher
+        const buttonFilters = document.createElement('button');
+        buttonFilters.textContent = project.category.name;
+        filtre.appendChild(buttonFilters);
+  
+        buttonFilters.addEventListener('click', () => {
+          filterName(project.categoryId);
+        });
+      }
+    });
+  
+    // Ajout du bouton "Tous" pour afficher l'ensemble des projets
+    const tousButton = document.createElement('button');
+    tousButton.textContent = "Tous";
+    filtre.appendChild(tousButton);
+  
+    tousButton.addEventListener('click', () => {
+      filterName(null);
+      //Null pour afficher tous les projets disponible
+      //Null = sans filtre appliqué
+    });
+  
+    // Afficher tous les projets au chargement de la page
+    displayProjects(datafiltre);
 });
-console.log()
