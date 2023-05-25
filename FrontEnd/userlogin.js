bf7544b4f67759d155c6a87386152854491db621
Mathilde Plancq <mathilde.plancq@gmail.com>
2023-05-25 11:09:23 +0200

    created webpage to edit index
    name : indexedit.html

--- FrontEnd/userlogin.js
+++ FrontEnd/userlogin.js
@@ -2,7 +2,8 @@
 const formulaire = document.querySelector('form')
 
 formulaire.addEventListener('submit',(evenement)=>{
-  evenement.preventDefault();
+  evenement.preventDefault();//Empêche le comportement par défaut
+  // le code récupère les valeurs du formulaire, effectue une requête HTTP asynchrone pour authentifier l'utilisateur et gère les conditions de la réponse
 
   const email = document.getElementById('email').value;
   const password = document.getElementById('password').value;
@@ -13,22 +14,24 @@ formulaire.addEventListener('submit',(evenement)=>{
   };
 
   fetch('http://localhost:5678/api/users/login', {
-    method:'POST',
+    method:'POST',  //option de la méthode de la requête
     headers: {
-      'Content-Type': 'application/json;charset=utf-8'
-    },
+      'Content-Type': 'application/json'
+    },//Indique au serveur le contenu de la requête au format JSON
     body: JSON.stringify(utilisateur)
+    //Convertit l'objet "utilisateur" en JSON pour l'envoyer dans la requête
   })
-  .then(APIreponse => {
-    if (APIreponse.ok) {// Condition si les données d'utilisateurs sont correctes
-      window.location.href = 'index.html'; 
-    } else {// Sinon message d'erreur
+  .then(APIreponse => {         //Effectue une fonction si promesse résolue
+    if (APIreponse.ok) {        //.ok est une propriété qui renvoie true or false 
+      //Condition si les données d'utilisateurs sont correctes
+      window.location.href = 'indexedit.html';  //Redirige l'utilisateur vers une URL
+    } else {                    // Sinon message d'erreur
       throw new Error('Erreur dans l\'identifiant ou le mot de passe');
     } 
   })
   .catch(erreur => {
     console.error(erreur);
-    
+
     const messageErreur = document.createElement('p');
     messageErreur.textContent = erreur.message;
     const zoneMessage = document.getElementById('message')
